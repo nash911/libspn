37916f948ffbc1f7ebbcf08ddb38472be1e5fe02
Avinash Ranganath <nash911@gmail.com>
2017-03-08 22:17:40 +0100

    Implement MPE path for Products

--- libspn/tests/test_nodes_products.py
+++ libspn/tests/test_nodes_products.py
@@ -50,77 +50,8 @@ class TestNodesProduct(unittest.TestCase):
         v1 = spn.ContVars(num_vars=3)
         v2 = spn.ContVars(num_vars=1)
 
-
-        # num_prods=2
-        #
-        # # Multiple inputs, multi-element batch
-        # test([v1, v2, v1, v2],
-        #      num_prods,
-        #      {v1: [[0.1, 0.2, 0.3],
-        #            [0.4, 0.5, 0.6]],
-        #       v2: [[0.7],
-        #            [0.8]]},
-        #      [[[0.1 * 0.2 * 0.3 * 0.7],
-        #       [0.4 * 0.5 * 0.6 * 0.8]],
-        #       [[0.1 * 0.2 * 0.3 * 0.7],
-        #        [0.4 * 0.5 * 0.6 * 0.8]]])
-        # test([(v1, [0, 2]), (v2, [0]), (v1, [0, 2]), (v2, [0])],
-        #      num_prods,
-        #      {v1: [[0.1, 0.2, 0.3],
-        #            [0.4, 0.5, 0.6]],
-        #       v2: [[0.7],
-        #            [0.8]]},
-        #      [[[0.1 * 0.3 * 0.7],
-        #       [0.4 * 0.6 * 0.8]],
-        #       [[0.1 * 0.3 * 0.7],
-        #        [0.4 * 0.6 * 0.8]]])
-        #
-        # # Single input with 1 value, multi-element batch
-        # test([v2, v2],
-        #      num_prods,
-        #      {v2: [[0.1],
-        #            [0.2]]},
-        #      [[[0.1],
-        #        [0.2]],
-        #       [[0.1],
-        #        [0.2]]])
-        # test([(v1, [1]), (v1, [1])],
-        #      num_prods,
-        #      {v1: [[0.01, 0.1, 0.03],
-        #            [0.02, 0.2, 0.04]]},
-        #      [[[0.1],
-        #        [0.2]],
-        #       [[0.1],
-        #        [0.2]]])
-        #
-        # # Multiple inputs, single-element batch
-        # test([v1, v2, v1, v2],
-        #      num_prods,
-        #      {v1: [[0.1, 0.2, 0.3]],
-        #       v2: [[0.7]]},
-        #      [[[0.1 * 0.2 * 0.3 * 0.7]],
-        #       [[0.1 * 0.2 * 0.3 * 0.7]]])
-        # test([(v1, [0, 2]), (v2, [0]), (v1, [0, 2]), (v2, [0])],
-        #      num_prods,
-        #      {v1: [[0.1, 0.2, 0.3]],
-        #       v2: [[0.7]]},
-        #      [[[0.1 * 0.3 * 0.7]],
-        #       [[0.1 * 0.3 * 0.7]]])
-        #
-        # # Single input with 1 value, single-element batch
-        # test([v2, v2],
-        #      num_prods,
-        #      {v2: [[0.1]]},
-        #      [[[0.1]],
-        #       [[0.1]]])
-        # test([(v1, [1]), (v1, [1])],
-        #      num_prods,
-        #      {v1: [[0.01, 0.1, 0.03]]},
-        #      [[[0.1]],
-        #       [[0.1]]])
-
         # Single Product node
-        num_prods=1
+        num_prods = 1
 
         # Multiple inputs, multi-element batch
         test([v1, v2],
@@ -177,7 +108,7 @@ class TestNodesProduct(unittest.TestCase):
              [[0.1]])
 
         # Multiple Product nodes
-        num_prods=2
+        num_prods = 2
 
         # Multiple inputs, multi-element batch
         test([v1, v2, v1, v2],
@@ -234,56 +165,44 @@ class TestNodesProduct(unittest.TestCase):
              [[0.1, 0.1]])
 
 
-    # def test_compute_valid(self):
-    #     """Calculating validity of Product"""
-    #     v12 = spn.IVs(num_vars=2, num_vals=4)
-    #     v34 = spn.ContVars(num_vars=2)
-    #     p1 = spn.Product((v12, [0, 5]))
-    #     p2 = spn.Product((v12, [0, 3]))
-    #     p3 = spn.Product((v12, [0, 5]), v34)
-    #     p4 = spn.Product((v12, [0, 3]), v34)
-    #     p5 = spn.Product((v12, [0, 5]), v34, (v12, 2))
-    #     self.assertTrue(p1.is_valid())
-    #     self.assertFalse(p2.is_valid())
-    #     self.assertTrue(p3.is_valid())
-    #     self.assertFalse(p4.is_valid())
-    #     self.assertFalse(p5.is_valid())
-
     def test_compute_mpe_path(self):
         v12 = spn.IVs(num_vars=2, num_vals=4)
         v34 = spn.ContVars(num_vars=2)
         v5 = spn.ContVars(num_vars=1)
-        p = spn.Product((v12, [0, 5]), v34, (v12, [3]), v5)
-        counts = tf.placeholder(tf.float32, shape=(None, 1))
+        p = spn.Products((v12, [0, 5]), v5, v34, (v12, [3]), num_prods=2)
+        counts = tf.placeholder(tf.float32, shape=(None, 2))
         op = p._compute_mpe_path(tf.identity(counts),
                                  v12.get_value(),
+                                 v5.get_value(),
                                  v34.get_value(),
-                                 v12.get_value(),
-                                 v5.get_value())
-        feed = [[0],
-                [1],
-                [2]]
+                                 v12.get_value())
+        feed = [[11, 12],
+                [21, 22],
+                [31, 32]]
         with tf.Session() as sess:
             out = sess.run(op, feed_dict={counts: feed})
         np.testing.assert_array_almost_equal(
-            out[0], np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
-                              [1., 0., 0., 0., 0., 1., 0., 0.],
-                              [2., 0., 0., 0., 0., 2., 0., 0.]],
+            out[0], np.array([[11., 0., 0., 0., 0., 11., 0., 0.],
+                              [21., 0., 0., 0., 0., 21., 0., 0.],
+                              [31., 0., 0., 0., 0., 31., 0., 0.]],
                              dtype=np.float32))
         np.testing.assert_array_almost_equal(
-            out[1], np.array([[0., 0.],
-                              [1., 1.],
-                              [2., 2.]],
+            out[1], np.array([[11.],
+                              [21.],
+                              [31.]],
                              dtype=np.float32))
         np.testing.assert_array_almost_equal(
-            out[2], np.array([[0., 0., 0., 0., 0., 0., 0., 0.],
-                              [0., 0., 0., 1., 0., 0., 0., 0.],
-                              [0., 0., 0., 2., 0., 0., 0., 0.]],
+            out[2], np.array([[12., 12.],
+                              [22., 22.],
+                              [32., 32.]],
                              dtype=np.float32))
+
         np.testing.assert_array_almost_equal(
-            out[3], np.array([[0.],
-                              [1.],
-                              [2.]],
+            out[3], np.array([[0., 0., 0., 12., 0., 0., 0., 0.],
+                              [0., 0., 0., 22., 0., 0., 0., 0.],
+                              [0., 0., 0., 32., 0., 0., 0., 0.]],
                              dtype=np.float32))
+
+
 if __name__ == '__main__':
     unittest.main()
