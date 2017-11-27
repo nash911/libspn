f99c5b408e1f4b09e40aa008bccc00c86619580f
Avinash Ranganath <nash911@gmail.com>
2017-11-27 20:16:32 +0100

    Add _compute_probable_path() to Products

--- libspn/graph/products.py
+++ libspn/graph/products.py
@@ -205,3 +205,9 @@ class Products(OpNode):
 
     def _compute_log_mpe_path(self, counts, *value_values, add_random=False, use_unweighted=False):
         return self._compute_mpe_path(counts, *value_values)
+
+    def _compute_probable_path(self, counts, *value_values):
+        return self._compute_mpe_path(counts, *value_values)
+
+    def _compute_log_probable_path(self, counts, *value_values):
+        return self._compute_mpe_path(counts, *value_values)
